41008e02710bc96dbaee357bdf0dd21196e6297a
ajay1264 <kambleajay146@gmail.com>
2024-09-06 21:49:03 +0530

    config of cors and cookieparser

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv'
 import connectDB from './db/index.js';
-
+import app from '../src/app.js'
 
 
 dotenv.config({
@@ -9,3 +9,13 @@ dotenv.config({
 
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, ()=> {
+        console.log(`Server is Running at port: ${process.env.PORt}`);
+        
+    })
+})
+.catch((err)=>{
+    console.log("MONGO DB CONNECTION FAILED", err);
+    
+})
