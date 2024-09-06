333c51c45e304b4cd7dffb3c5ea5274af2b31ff1
ajay1264 <kambleajay146@gmail.com>
2024-09-06 20:50:13 +0530

    connected to mongodb atlas

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,3 +1,11 @@
-import mongoose from 'mongoose'
-import { DB_NAME } from './constants.js';
+import dotenv from 'dotenv'
+import connectDB from './db/index.js';
 
+
+
+dotenv.config({
+    path: './env'
+})
+
+
+connectDB()
