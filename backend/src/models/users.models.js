8d0d1a7bd23b55d9143c163b07e2e9cab05929a3
ajay1264 <kambleajay146@gmail.com>
2024-09-12 16:48:14 +0530

    multer added

--- backend/src/models/users.models.js
+++ backend/src/models/users.models.js
@@ -33,6 +33,42 @@ const userSchema = mongoose.Schema({
     }
 },{timestamps: true})
 
+//for hashing password
+userSchema.pre("save", async function (next) {
+    if(!this.isModified("password")) return (next)
+    this.password = bcrypt.hash(this.password, 10)
+    next()
+} )
+//custom method
+
+userSchema.methods.isPasswordCorrect = async function (password){
+ return await  bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function() {
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+userSchema.methods.generateRefreshToken = function() {
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
 const User = mongoose.model("User", userSchema)
 
 export default User
